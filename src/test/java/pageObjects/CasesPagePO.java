7198153a53cb96b715df81d51da387e8144aaa5a
ankitsinghalcode <aasinghal1@gmail.com>
2024-08-03 14:34:34 +0530

    Added create Cases Page Object file

---
+++ src/test/java/pageObjects/CasesPagePO.java
@@ -0,0 +1,12 @@
+package pageObjects;
+
+
+public class CasesPagePO {
+
+    //WebElement casesIcons= driver.findElement(By.xpath(casesIcon));
+
+    public static String casesIcon = "(//span[normalize-space()='Cases'])[1]";
+    public static String caseAddIcon= "(//i[@aria-hidden='true'])[6]";
+    public static String createNewCase= "(//span[@class='selectable '])[1]";
+
+}
